30f92fe7428fd01c096a5735274a59c7832f645b
GilSer <gilyan74@bk.ru>
2024-06-29 22:03:38 +0200

    corrected

--- src/Main2.java
+++ src/Main2.java
@@ -1,6 +1,11 @@
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 import java.util.Optional;
+
+
+import static java.util.stream.Collectors.toList;
+
 
 public class Main2 {
     public static void main(String[] args) {
@@ -15,8 +20,14 @@ public class Main2 {
                 .map(str -> str.length())
                 .filter(x -> x < 15)
                 .max(Integer::compareTo);
-                
         System.out.println(result);
+
+        List<String> string = strings.stream()
+                .filter(str -> (str.length() < 15))
+                .collect(toList())
+                .stream().max(Comparator.comparing(String::length))
+                .stream().toList();
+        System.out.println(string);
 
 
     }
